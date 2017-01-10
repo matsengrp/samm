56d57d957f8bfa3e5e31b7262d74b61725526eed
D. Shaw <wahsadivad@gmail.com>
2017-01-10 10:20:44 -0800

    use seed

--- shmulate_driver.r
+++ shmulate_driver.r
@@ -1,6 +1,12 @@
-## hack to simulate stuff like yale group volume 2!
+# Run shmulate given command line arguments
 source('shmulate/R/SHMulate.R')
 source('shmulate/R/SHMulate_Functions.R')
+
+# Packages needed:
+# igraph
+# plyr
+# seqinr
+# shazam
 
 arg <- commandArgs(TRUE)                       
 n.taxa <- as.numeric(arg[1])
@@ -8,6 +14,8 @@ seq.file <- arg[2]
 germline <- arg[3]
 group <- arg[4]
 n.mutes <- arg[5]
+
+set.seed(arg[6])
 
 glc <- tolower(s2c(germline))
 n.nuc <- length(glc)
@@ -22,5 +30,5 @@ dat <- t(sapply(1:nrow(seqs), function(u) s2c(tolower(seqs[u,'sequence']))))
 outdat <- lapply(1:nrow(dat), function(u) dat[u,])
 
 # output file should be same as input file
-write.fasta(sequences = outdat, file.out = seq.file, names = paste('Sequence', 1:n.taxa, sep=''))
+write.fasta(sequences = outdat, file.out = seq.file, names = paste(group, '-Sequence', 1:n.taxa, sep=''))
 
