995719b41dc92e56963ce8e415380b1de8b44ace
Jean Feng <jean.feng@gmail.com>
2017-01-19 15:55:29 -0800

    add parallelization to e-step

--- mcmc_em.py
+++ mcmc_em.py
@@ -1,3 +1,4 @@
+import time
 import numpy as np
 
 from models import *
@@ -5,31 +6,47 @@ from feature_generator import SubmotifFeatureGenerator
 # from mutation_order_sampler import MutationOrderSampler
 from mutation_order_gibbs import MutationOrderGibbsSampler
 from survival_problem import SurvivalProblem
+from sampler_collection import SamplerCollection
 
 class MCMC_EM:
-    def __init__(self, observed_data, feat_generator, base_num_e_samples=10, burn_in=10):
+    def __init__(self, observed_data, feat_generator, sampler_cls, base_num_e_samples=10, burn_in=10, num_threads=1):
         self.observed_data = observed_data
         self.feat_generator = feat_generator
         self.base_num_e_samples = base_num_e_samples
         self.burn_in = burn_in
+        self.sampler_cls = sampler_cls
+        self.num_threads = num_threads
 
-    def run(self, max_iters=10):
+    def run(self, max_iters=10, verbose=False):
         # initialize theta vector
         theta = np.random.randn(self.feat_generator.feature_vec_len)
         # stores the initialization for the gibbs samplers for the next iteration's e-step
-        init_orders_for_iter = [obs_seq.mutation_pos_dict.keys() for obs_seq in self.observed_data]
+        init_orders = [obs_seq.mutation_pos_dict.keys() for obs_seq in self.observed_data]
         for i in range(max_iters):
-            num_e_samples = (i + 1) * self.base_num_e_samples
             # do E-step
-            samples_for_obs_seq = [
-                self.get_e_samples(obs_seq, theta, init_order, num_e_samples) for obs_seq, init_order in zip(self.observed_data, init_orders_for_iter)
-            ]
+            num_e_samples = (i + 1) * self.base_num_e_samples
+            sampler_collection = SamplerCollection(
+                self.observed_data,
+                theta,
+                self.sampler_cls,
+                self.feat_generator,
+                self.num_threads,
+            )
+
+            sampled_orders_list = sampler_collection.get_samples(
+                init_orders,
+                num_e_samples,
+                self.burn_in,
+            )
+            # the last sampled mutation order from each list
             # use this iteration's sampled mutation orders as initialization for the gibbs samplers next cycle
-            init_orders_for_iter = [samples[-1].mutation_order for samples in samples_for_obs_seq]
-            e_step_samples = [s for s in samples for samples in samples_for_obs_seq]
+            init_orders = [sampled_orders[-1].mutation_order for sampled_orders in sampled_orders_list]
+            # flatten the list of samples to get all the samples
+            e_step_samples = [o for orders in sampled_orders_list for o in orders]
+
             # Do M-step
             problem = SurvivalProblem(e_step_samples)
-            theta, exp_log_lik = problem.solve(self.feat_generator)
+            theta, exp_log_lik = problem.solve(self.feat_generator, verbose=verbose)
         return theta
 
     def get_e_samples(self, obs_seq, theta, init_order, num_samples):
--- test.py
+++ test.py
@@ -9,6 +9,10 @@ from mutation_order_gibbs import MutationOrderGibbsSampler
 
 np.random.seed(10)
 
+num_threads = 2
+seq_length = 15
+num_extra_seq = 0
+
 feat_generator = SubmotifFeatureGenerator()
 
 # True vector
@@ -22,6 +26,10 @@ start_seqs = [
     "taagggaaaaaaaaaatgggggttt",
     "tttaaaaaagggggggggttgggttaaat",
 ]
+for i in range(num_extra_seq):
+    start_seqs.append(
+        get_random_dna_seq(seq_length)
+    )
 
 full_data_samples = [simulator.simulate(s, 0.1) for s in start_seqs]
 obs_data = [s.obs_seq_mutation for s in full_data_samples]
@@ -30,7 +38,10 @@ for i, obs in enumerate(obs_data):
         print k, obs.start_seq[k], "=>", v
 
 # Run em
-em_algo = MCMC_EM(obs_data, feat_generator, base_num_e_samples=10)
+em_algo = MCMC_EM(
+    obs_data,
+    feat_generator,
+    MutationOrderGibbsSampler,
+)
 theta = em_algo.run()
 print "theta", theta
-print theta
