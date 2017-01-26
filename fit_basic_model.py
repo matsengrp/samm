7843387fdbc27e4a6879448bb6678b02d5c08afa
D. Shaw <wahsadivad@gmail.com>
2017-01-26 14:12:11 -0800

    fit basic order-free model

---
+++ fit_basic_model.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Fit a basic, order-free model by counting
+"""
+
+import sys
+import argparse
+import os
+import os.path
+import csv
+import pickle
+
+import numpy as np
+from models import ObservedSequenceMutations
+from mcmc_em import MCMC_EM
+from feature_generator import SubmotifFeatureGenerator
+from mutation_order_gibbs import MutationOrderGibbsSampler
+from common import *
+
+def parse_args():
+    ''' parse command line arguments '''
+
+    parser = argparse.ArgumentParser(description=__doc__)
+
+    parser.add_argument('--seed',
+        type=int,
+        help='rng seed for replicability',
+        default=1533)
+    parser.add_argument('--input-file',
+        type=str,
+        help='sequence data in csv',
+        default='_output/seqs.csv')
+    parser.add_argument('--input-genes',
+        type=str,
+        help='genes data in csv',
+        default='_output/genes.csv')
+    parser.add_argument('--num-threads',
+        type=str,
+        help='number of threads to use during E-step',
+        default=4)
+    parser.add_argument('--motif-len',
+        type=str,
+        help='length of motif (must be odd)',
+        default=5)
+
+    args = parser.parse_args()
+
+    assert(args.motif_len % 2 == 1 and args.motif_len > 1)
+
+    return args
+
+def main(args=sys.argv[1:]):
+    args = parse_args()
+    np.random.seed(args.seed)
+    feat_generator = SubmotifFeatureGenerator(submotif_len=args.motif_len)
+
+    gene_dict, obs_data = read_gene_seq_csv_data(args.input_genes, args.input_file)
+
+    motif_list = SubmotifFeatureGenerator.get_motif_list(args.motif_len)
+    motif_list.append('EDGES')
+
+    mutations = {}
+
+    # TODO: this doesn't do anything clever with overlapping mutations. Should we
+    # double count them?
+    for obs_seq in obs_data:
+        mutated_positions = obs_seq.mutation_pos_dict.keys()
+        germline_motifs = feat_generator.create_for_sequence(obs_seq.start_seq)
+        for mut_pos in mutated_positions:
+            for mutation in germline_motifs[mut_pos]:
+                if motif_list[mutation] in mutations:
+                    mutations[motif_list[mutation]] += 1
+                else:
+                    mutations[motif_list[mutation]] = 1
+
+    with open(args.out_file, "w") as f:
+        pickle.dump(mutations, f)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
