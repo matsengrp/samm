cf595d14399727dc83cfefa4a2118ea32b075cd9
D. Shaw <wahsadivad@gmail.com>
2017-02-13 16:50:56 -0800

    more useful data file

--- matsen_grp_data.py
+++ matsen_grp_data.py
@@ -3,53 +3,54 @@ Modules for processing real data
 
 I'm not sure about the privacy of some of these data sets so I'm putting
 all of this in a separate file.
+
 """
 
-from itertools import product
+import os
+import re
+import fnmatch
 
-LAURA_DATA_PATH = '/fh/fast/matsen_e/processed-data/partis/laura-mb-2016-12-22/v8'
-KATE_DATA_PATH = '/fh/fast/matsen_e/processed-data/partis/laura-mb-2016-12-22/v8'
+PARTIS_PROCESSED_PATH = '/fh/fast/matsen_e/processed-data/partis'
 
-def get_atreca_seeded_data(pth, chain='h', ig_class='G'):
+LAURA_DATA_PATH = PARTIS_PROCESSED_PATH+'/laura-mb-2016-12-22/v8'
+KATE_DATA_PATH = PARTIS_PROCESSED_PATH+'/kate-qrs-2016-09-09/v8'
+
+def get_seeded_data(pth, chain='h', ig_class='G', pid=None):
     """
     @param pth: prefix path to partis-processed data
     @param chain: 'h', 'k' or 'l'
     @param ig_class: 'G' or 'M' for heavy chain, o/w computed as chain.upper()
+    @param pid: patient ID; if None then use all annotations found
 
     @returns: two lists, one of paths to annotations files and one of corresponding paths
     to inferred germlines
 
-    Notes:
-    Data in LAURA_DATA_PATH and KATE_DATA_PATH are in similar forms, as are other data sets
-    obtained via Atreca. Typically we have
-    
-    BF520.{0}-ig{1}/Hs-LN-{2}-5RACE-Ig{3}/
-
-    where
-
-    {0}: seed_id from 1--10 if chain is 'h' o/w in {3,10} if chain is 'l' o/w in 1--10 \ {3,10}
-    {1}: chain in {'h', 'k', 'l'}
-    {2}: run_id in {'C', 'D'}
-    {3}: class in {'G', 'M'} if chain is 'h' o/w equals chain.upper()
     """
-
-    seeds = range(1,11)
-    runs = ['C', 'D']
-    if chain=='k':
-        ig_class = chain.upper()
-        seeds = [1,2,4,5,6,7,8,9]
-    elif chain=='l':
-        ig_class = chain.upper()
-        seeds = [3,10]
-
-    file_path = pth + '/seeds/BF520.{0}-ig{1}/Hs-LN-{2}-5RACE-Ig{3}/partition-cluster-annotations.csv'
-    gl_path = pth + '/Hs-LN-{0}-5RACE-Ig{1}/hmm/germline-sets'
 
     annotations_paths = []
     germline_paths = []
-    for seed, run in product(seeds, runs):
-        annotations_paths.append(file_path.format(seed, chain, run, ig_class))
-        germline_paths.append(gl_path.format(run, ig_class, chain))
+    meta = {}
+    regex = re.compile(r'^(?P<pid>[^.]*).(?P<seedid>[0-9]*)-(?P<chain>[^/]*)/(?P<timepoint>[^.]*)')
+
+    if chain != 'h':
+        ig_class = chain.upper()
+
+    for root, dirnames, filenames in os.walk(pth):
+        for fname in fnmatch.filter(filenames, 'partition-cluster-annotations.csv'):
+            # regex jazz hands
+            # remove initial path
+            m = regex.match(re.sub(pth+'/seeds/', '', root))
+            if m:
+                meta = m.groupdict()
+                # I want a regex to do this but I couldn't figure it out
+                meta['timepoint'] = re.sub('-100k', '', meta['timepoint'])
+                # the chain is the last character of meta['chain'] and the ig_class is the last character of timepoint
+                subset = (meta['chain'][-1].lower() == chain and
+                          meta['timepoint'][-1].upper() == ig_class and
+                          (meta['pid'] == pid or pid == None))
+                if subset:
+                    annotations_paths.append(os.path.join(root, fname))
+                    germline_paths.append(os.path.join(pth, meta['timepoint'], 'hmm/germline-sets'))
 
     return annotations_paths, germline_paths
 
