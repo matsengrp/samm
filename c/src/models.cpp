fa961905ba6e964f4efe04e08c2bd05b5cebbe00
Jean Feng <jean.feng@gmail.com>
2017-02-19 16:37:18 -0800

    shared ptrs were fine

--- c/src/models.cpp
+++ c/src/models.cpp
@@ -23,18 +23,6 @@ OrderedMutationSteps::OrderedMutationSteps(VectorOrder ord_v) {
   mut_steps.resize(num_steps);
 }
 
-OrderedMutationSteps::OrderedMutationSteps(
-  int num_s,
-  VectorOrder order_v,
-  vector<shared_ptr<MutationStep> > m_vec
-): num_steps(num_s), order_vec(order_v) {
-  for (auto i: m_vec) {
-    mut_steps.push_back(
-      make_shared<MutationStep>(*i)
-    );
-  }
-}
-
 void OrderedMutationSteps::set(int step_i, shared_ptr<MutationStep> mut_step) {
   mut_steps[step_i] = mut_step;
 }
