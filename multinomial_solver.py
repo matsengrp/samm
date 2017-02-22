6d27537afb5a54cb5ae7c007521523904f515b22
Jean Feng <jean.feng@gmail.com>
2017-02-22 06:20:11 -0800

    fuse motif comment - no solution

--- multinomial_solver.py
+++ multinomial_solver.py
@@ -28,7 +28,6 @@ class MultinomialSolver:
         for motif_idx1, m1 in enumerate(motif_list):
             if motif_idx1 not in already_fused_motifs:
                 fused_idxs = [motif_idx1]
-
                 # now look for other motifs that it is fused with
                 for index_delta, m2 in enumerate(motif_list[motif_idx1 + 1:]):
                     # Must have the same center nucleotide
@@ -40,9 +39,17 @@ class MultinomialSolver:
                     if motif_idx2 in already_fused_motifs:
                         continue
 
+                    # TODO: This a bit more complex! If they differ by one character and are fused
+                    # but if they differ by more than two characters but the other characters are fused,
+                    # You also want to fuse them...
+                    # Another option is just to fuse anything with close theta values.
+                    # This assumes that it is really unlikely to have theta values really close
+                    # if there is no connection between the theta values
+                    # And what about if theta is zero? Do we fuse them too??
+
                     # Must differ by no more than one character
-                    if get_idx_differ_by_one_character(m1, m2) is None:
-                        continue
+                    # if get_idx_differ_by_one_character(m1, m2) is None:
+                    #     continue
 
                     # Must have similar theta values
                     if np.isclose(theta[motif_idx1], theta[motif_idx2]):
@@ -50,11 +57,12 @@ class MultinomialSolver:
 
                 motif_classes.append(fused_idxs)
 
-                motif_class_idx = len(fused_motif_set) - 1
+                motif_class_idx = len(motif_classes) - 1
                 for i in fused_idxs:
                     motif_to_motif_class_dict[i] = motif_class_idx
+                    already_fused_motifs.add(i)
 
-        num_motif_classes = len(fused_motif_set)
+        num_motif_classes = len(motif_classes)
         # count number of times we see a mutation to a target nucleotide from a motif class
         target_mutation_count = np.matrix([[0.] * NUM_NUCLEOTIDES] * num_motif_classes)
         # count number of times we see a motif class
@@ -64,11 +72,11 @@ class MultinomialSolver:
             for mut_pos, mut_nuc in obs_seq.mutation_pos_dict.iteritems():
                 feature_idx = germline_motifs[mut_pos]
                 motif_class_idx = motif_to_motif_class_dict[feature_idx]
-                motif_count[motif_class_idx] += 1.
+                motif_class_count[motif_class_idx] += 1.
                 target_mutation_count[motif_class_idx, NUCLEOTIDE_DICT[mut_nuc]] += 1.
 
         # Now get the MLE of the multinomial distributions
-        proportions = np.matrix([[0.] * NUM_NUCLEOTIDES] * num_motif_classes)
+        proportions = np.matrix([[0.] * NUM_NUCLEOTIDES] * len(motif_list))
         for i, (motif_class, count) in enumerate(zip(motif_classes, motif_class_count)):
             if count > 0:
                 # motifs in the same motif class have the same proportions
