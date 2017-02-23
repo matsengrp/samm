2ac620aa0b23dfb6000f047d573e532583aec080
Jean Feng <jean.feng@gmail.com>
2017-02-23 14:16:09 -0800

    use sparse matrices instead

--- models.py
+++ models.py
@@ -63,9 +63,9 @@ class ObservedSequenceMutations:
         self.feat_matrix_start = feat_matrix
 
         num_feats = feat_matrix.shape[1]
-        self.feat_counts_flat = np.zeros(num_feats, dtype=int)
+        self.feat_counts = np.zeros((num_feats, 1), dtype=int)
         for pos, feat in feat_dict.iteritems():
-            self.feat_counts_flat[feat] += 1
+            self.feat_counts[feat] += 1
 
     def __str__(self):
         return "Seq %s, Mutations %s" % (
