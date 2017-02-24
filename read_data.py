1f900ba6ed9ab3579f9cdcf085be8e65d93f63a8
D. Shaw <wahsadivad@gmail.com>
2017-02-23 17:20:53 -0800

    write intermediate file and randomly choose item from clone

--- read_data.py
+++ read_data.py
@@ -17,7 +17,8 @@ from models import ObservedSequenceMutations
 GERMLINE_PARAM_FILE = PARTIS_PATH + '/data/germlines/human/h/ighv.fasta'
 SAMPLE_PARTIS_ANNOTATIONS = PARTIS_PATH + '/test/reference-results/partition-new-simu-cluster-annotations.csv'
 
-def read_partis_annotations(annotations_file_names, chain='h', use_v=True, species='human', use_np=True, inferred_gls=None, motif_len=1, output_presto=False):
+# TODO: file to convert presto dataset to ours? just correspondence between headers should be enough?
+def write_partis_data_from_annotations(output_genes, output_seqs, annotations_file_names, chain='h', use_v=True, species='human', use_np=True, inferred_gls=None, motif_len=1, output_presto=False, impute_ancestors=False):
     """
     Function to read partis annotations csv
 
@@ -28,9 +29,7 @@ def read_partis_annotations(annotations_file_names, chain='h', use_v=True, speci
     @param use_np: use nonproductive sequences only
     @param inferred_gls: list of paths to partis-inferred germlines
 
-    TODO: convert to presto headers for presto...
-
-    @return gene_dict, obs_data
+    @write genes to output_genes and seqs to output_seqs
     """
 
     if not isinstance(annotations_file_names, list):
@@ -47,12 +46,12 @@ def read_partis_annotations(annotations_file_names, chain='h', use_v=True, speci
         glfo = glutils.read_glfo(PARTIS_PATH + '/data/germlines/' + species, chain=chain)
         inferred_gls = [None] * len(annotations_file_names)
 
-    gene_dict = {}
     if output_presto:
-        prestoheader = utils.presto_headers.values()
-        obs_data = pd.DataFrame(columns=prestoheader)
+        seq_header = utils.presto_headers.values()
     else:
-        obs_data = []
+        seq_header = ['germline_name', 'sequence_name', 'sequence']
+
+    obs_data = pd.DataFrame(columns=seq_header)
 
     seqs_col = 'v_qr_seqs' if use_v else 'seqs'
     gene_col = 'v_gl_seq' if use_v else 'naive_seq'
@@ -66,31 +65,71 @@ def read_partis_annotations(annotations_file_names, chain='h', use_v=True, speci
         # return all sequences
         good_seq = lambda seqs: [True for seq in seqs[seqs_col]]
 
-    for annotations_file, germline_file in zip(annotations_file_names, inferred_gls):
-        if germline_file is not None:
-            glfo = germlines[germline_file]
-        with open(annotations_file, "r") as csvfile:
-            reader = csv.DictReader(csvfile)
-            for idx, line in enumerate(reader):
-                # add goodies from partis
-                utils.process_input_line(line)
-                utils.add_implicit_info(glfo, line)
-                # for now just use V gene for ID
-                key = 'clone{}-{}'.format(*[idx, line['v_gene']])
-                gene_dict[key] = line[gene_col].lower()
-                good_seqs = [seq for seq, cond in zip(line[seqs_col], good_seq(line)) if cond]
-                # TODO: impute ancestors
-                end_seq = random.choice(good_seqs)
-                # process sequences
-                gl_seq, ch_seq = process_degenerates_and_impute_nucleotides(line[gene_col].lower(), end_seq.lower(), motif_len)
-                obs_data.append(
-                    ObservedSequenceMutations(
-                        start_seq=gl_seq,
-                        end_seq=ch_seq,
-                        motif_len=motif_len,
-                    )
-                )
-    return gene_dict, obs_data
+    with open(output_genes, 'w') as genes_file, open(output_seqs, 'w') as seqs_file:
+        gene_writer = csv.DictWriter(genes_file, ['germline_name', 'germline_sequence'])
+        gene_writer.writeheader()
+        seq_writer = csv.DictWriter(seqs_file, seq_header)
+        seq_writer.writeheader()
+        for data_idx, (annotations_file, germline_file) in enumerate(zip(annotations_file_names, inferred_gls)):
+            if germline_file is not None:
+                glfo = germlines[germline_file]
+            with open(annotations_file, "r") as csvfile:
+                reader = csv.DictReader(csvfile)
+                for idx, line in enumerate(reader):
+                    # add goodies from partis
+                    utils.process_input_line(line)
+                    utils.add_implicit_info(glfo, line)
+                    good_seq_idx = [i for i, is_good in enumerate(good_seq(line)) if is_good]
+                    if not good_seq_idx:
+                        # no nonproductive sequences... skip
+                        continue
+                    filtered_line = line.copy()
+                    if impute_ancestors:
+                        # dress each sequence with its ancestor and put into column gene_col using all seqs
+                        # then filter based on good_seq()
+                        filtered_line = compute_ancestors(filtered_line, gene_col, seqs_col)
+                        for col in [gene_col, seqs_col, 'unique_ids']:
+                            filtered_line[col] = []
+                            for good_idx in good_seq_idx:
+                                filtered_line[col].append(line[col][good_idx])
+                        # TODO: this will repeat some germlines since they're shared between sequences... oh well
+                    else:
+                        # otherwise pick a random good_seq()
+                        random_idx = random.choice(good_seq_idx)
+                        for col in [seqs_col, 'unique_ids']:
+                            filtered_line[col] = line[col][random_idx]
+                        gl_name = 'clone{}-{}-{}'.format(*[data_idx, idx, line['v_gene']])
+                        gene_writer.writerow({'germline_name': gl_name,
+                            'germline_sequence': filtered_line[gene_col].lower()})
+                        seq_writer.writerow(get_seq_line(filtered_line, seqs_col, gl_name, output_presto))
+
+def get_seq_line(line, seqs_col, gl_name, output_presto):
+    """
+    @param lines: partis sequence/germline data
+    @param output_presto: whether to output presto data
+    @param gene_col: column of lines where germline gene sits
+    @param seqs_col: column of lines where sequence sits
+
+    @return list of dicts containing sequences and ancestors
+    """
+
+    if output_presto:
+        # output with presto data/headers
+        datum = utils.convert_to_presto_headers(line)
+    else:
+        # output with our data/headers
+        datum = {'germline_name': gl_name,
+            'sequence_name': '-'.join([gl_name, line['unique_ids']]),
+            'sequence': line[seqs_col].lower()}
+
+    return datum
+
+def compute_ancestors(line, gene_col, seqs_col):
+    """
+    Compute ancestral states via maximum parsimony
+    """
+
+    return line
 
 def read_gene_seq_csv_data(gene_file_name, seq_file_name, motif_len=1):
     """
@@ -109,8 +148,6 @@ def read_gene_seq_csv_data(gene_file_name, seq_file_name, motif_len=1):
     with open(seq_file_name, "r") as seq_csv:
         seq_reader = csv.reader(seq_csv, delimiter=",")
         seq_reader.next()
-        # TODO: randomly sample from cluster
-        # TODO: impute ancestors
         for row in seq_reader:
             # process sequences
             start_seq, end_seq = process_degenerates_and_impute_nucleotides(gene_dict[row[0]], row[2].lower(), motif_len)
--- test/test_partis_input.py
+++ test/test_partis_input.py
@@ -9,6 +9,10 @@ class Input_Data_TestCase(unittest.TestCase):
         Test partis input functions
         """
 
+        temp_genes = '_output/genes.csv'
+        temp_seqs = '_output/seqs.csv'
+        motif_len = 3
+
         for dataset in [LAURA_DATA_PATH, KATE_DATA_PATH]:
             print 'dataset: ', dataset
             print 'chain, class, nonproductive reads'
@@ -17,17 +21,20 @@ class Input_Data_TestCase(unittest.TestCase):
                     # check IgG
                     igclass = 'G'
                     annotations, germlines = get_paths_to_partis_annotations(dataset, chain=chain, ig_class=igclass)
-                    genes, seqs = read_partis_annotations(annotations, inferred_gls=germlines, chain=chain)
+                    write_partis_data_from_annotations(temp_genes, temp_seqs, annotations, inferred_gls=germlines, chain=chain)
+                    genes, seqs = read_gene_seq_csv_data(temp_genes, temp_seqs, motif_len)
                     print chain, igclass, len(seqs)
 
                     # check IgM
                     igclass = 'M'
                     annotations, germlines = get_paths_to_partis_annotations(dataset, chain=chain, ig_class=igclass)
-                    genes, seqs = read_partis_annotations(annotations, inferred_gls=germlines, chain=chain)
+                    write_partis_data_from_annotations(temp_genes, temp_seqs, annotations, inferred_gls=germlines, chain=chain)
+                    genes, seqs = read_gene_seq_csv_data(temp_genes, temp_seqs, motif_len)
                     print chain, igclass, len(seqs)
                 else:
                     # check light chain
                     igclass = chain.upper()
                     annotations, germlines = get_paths_to_partis_annotations(dataset, chain=chain)
-                    genes, seqs = read_partis_annotations(annotations, inferred_gls=germlines, chain=chain)
+                    write_partis_data_from_annotations(temp_genes, temp_seqs, annotations, inferred_gls=germlines, chain=chain)
+                    genes, seqs = read_gene_seq_csv_data(temp_genes, temp_seqs, motif_len)
                     print chain, igclass, len(seqs)
