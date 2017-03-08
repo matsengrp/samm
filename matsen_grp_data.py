7328c4f09bdaed6a4cf1f791052c4bd23f209102
D. Shaw <wahsadivad@gmail.com>
2017-03-07 16:11:24 -0800

    add cui data script

--- matsen_grp_data.py
+++ matsen_grp_data.py
@@ -14,8 +14,9 @@ PARTIS_PROCESSED_PATH = '/fh/fast/matsen_e/processed-data/partis'
 
 LAURA_DATA_PATH = PARTIS_PROCESSED_PATH+'/laura-mb-2016-12-22/v8'
 KATE_DATA_PATH = PARTIS_PROCESSED_PATH+'/kate-qrs-2016-09-09/v8'
+CUI_DATA_PATH = PARTIS_PROCESSED_PATH+'/cui-et-al-2017-01-17'
 
-def get_paths_to_partis_annotations(pth, chain='h', ig_class='G', pid=None):
+def get_paths_to_partis_annotations_for_kate_and_laura(pth, chain='h', ig_class='G', pid=None):
     """
     @param pth: prefix path to partis-processed data
     @param chain: 'h', 'k' or 'l'
@@ -51,6 +52,31 @@ def get_paths_to_partis_annotations(pth, chain='h', ig_class='G', pid=None):
                 if subset:
                     annotations_paths.append(os.path.join(root, fname))
                     germline_paths.append(os.path.join(pth, meta['timepoint'], 'hmm/germline-sets'))
+
+    return annotations_paths, germline_paths
+
+def get_paths_to_partis_annotations(pth, chain='k'):
+    """
+    @param pth: prefix path to partis-processed data
+    @param chain: 'h', 'k' or 'l'
+
+    @returns: two lists, one of paths to annotations files and one of corresponding paths
+    to inferred germlines
+
+    """
+
+    annotations_paths = []
+    germline_paths = []
+
+    for root, dirnames, filenames in os.walk(pth):
+        for fname in fnmatch.filter(filenames, '*-cluster-annotations.csv'):
+            # determine chain
+            run_info = fname.split('_')
+            run_chain = 'k' if 'IGK' in run_info else 'l'
+            if run_chain == chain:
+                run_dir = re.sub('-10k-cluster-annotations.csv', '', fname)
+                annotations_paths.append(os.path.join(root, fname))
+                germline_paths.append(os.path.join(pth, run_dir, 'hmm/germline-sets'))
 
     return annotations_paths, germline_paths
 
