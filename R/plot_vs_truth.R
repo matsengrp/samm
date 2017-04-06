226f48178ac4af63712bca5fe9e89fc7460d99a0
D. Shaw <wahsadivad@gmail.com>
2017-04-05 17:23:45 -0700

    simple scatter plot

---
+++ R/plot_vs_truth.R
@@ -0,0 +1,36 @@
+# plot fitted vs truth
+
+arg <- commandArgs(TRUE)
+true_data_path <- arg[1]
+fitted_data_path <- arg[2]
+plot_path <- arg[3]
+plot_title <- arg[4]
+
+getLogMutabilitiesFromPath <- function(path) {
+    # what it says on the tin
+    raw_data <- read.table(path, sep=',')
+    log_mutabilities <- unlist(raw_data['V2'])
+    names(log_mutabilities) <- unlist(raw_data['V1'])
+    log_mutabilities <- log_mutabilities[!grepl("N", names(log_mutabilities))]
+    return(log_mutabilities)
+}
+
+# We're plotting mutabilities, so we exponentiate and scale them
+true_log_mutabilities <- getLogMutabilitiesFromPath(true_data_path)
+true_scaled <- true_log_mutabilities -
+    log(sum(exp(true_log_mutabilities), na.rm=TRUE))
+
+fitted_log_mutabilities <- getLogMutabilitiesFromPath(fitted_data_path)
+fitted_scaled <- fitted_log_mutabilities -
+    log(sum(exp(fitted_log_mutabilities), na.rm=TRUE))
+
+png(plot_path)
+
+plot(true_scaled,
+     fitted_scaled,
+     xlab = 'True',
+     ylab = 'Fitted',
+     main = plot_title)
+abline(0,1)
+
+dev.off()
