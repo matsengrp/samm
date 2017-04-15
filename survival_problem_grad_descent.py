fix gradient calculation

survival_problem_grad_descent.py
@@ -241,11 +241,10 @@ class SurvivalProblemCustom(SurvivalProblem):
             denominators.append(new_denom)
             prev_denom = new_denom
 
-        numerators = theta[sample_data.mutating_pos_feat_vals_rows, 0, None]
+        numerators = theta[sample_data.mutating_pos_feat_vals_rows, 0]
         if per_target_model:
             numerators = numerators + theta[sample_data.mutating_pos_feat_vals_rows, sample_data.mutating_pos_feat_vals_cols]
-        print "denominators", denominators
-        print "numerators", numerators
+
         log_lik = numerators.sum() - np.log(denominators).sum()
         return log_lik
 
@@ -259,20 +258,28 @@ class SurvivalProblemCustom(SurvivalProblem):
         @param theta: the theta to evaluate the gradient at
         @param sample_data: SamplePrecalcData
         """
-        pos_exp_theta = np.exp(sample_data.obs_seq_mutation.feat_matrix_start.dot(theta))
+        merged_thetas = theta[:,0, None]
+        if per_target_model:
+            merged_thetas = merged_thetas + theta[:,1:]
+        pos_exp_theta = np.exp(sample_data.obs_seq_mutation.feat_matrix_start.dot(merged_thetas))
         prev_denom = pos_exp_theta.sum()
         denominators = [prev_denom]
 
         prev_risk_group_grad = sample_data.obs_seq_mutation.feat_matrix_start.transpose().dot(pos_exp_theta)
+        if per_target_model:
+            prev_risk_group_grad = np.hstack([prev_risk_group_grad.sum(axis=1, keepdims=True), prev_risk_group_grad])
         risk_group_grads = [prev_risk_group_grad/prev_denom]
         risk_group_grad_tot = prev_risk_group_grad/prev_denom
         for pos_feat_matrix, pos_feat_matrixT, features_sign_update in zip(sample_data.features_per_step_matrices, sample_data.features_per_step_matricesT, sample_data.features_sign_updates):
-            exp_thetas = np.exp(pos_feat_matrix.dot(theta))
+            exp_thetas = np.exp(pos_feat_matrix.dot(merged_thetas))
             signed_exp_thetas = np.multiply(exp_thetas, features_sign_update)
 
             new_denom = prev_denom + signed_exp_thetas.sum()
+
             denominators.append(new_denom)
             grad_update = pos_feat_matrixT.dot(signed_exp_thetas)
+            if per_target_model:
+                grad_update = np.hstack([grad_update.sum(axis=1, keepdims=True), grad_update])
             risk_group_grad = prev_risk_group_grad + grad_update
             risk_group_grads.append(risk_group_grad/new_denom)
             risk_group_grad_tot += risk_group_grad/new_denom

test/test_survival_prob_grad_descent.py
@@ -24,13 +24,19 @@ class Survival_Problem_Gradient_Descent_TestCase(unittest.TestCase):
 
         cls.sample_hier = ImputedSequenceMutations(cls.feat_gen_hier.create_base_features(obs_seq_mut), cls.mutation_order)
 
-    def _compare_grad_calculation(self, feat_gen, sample, theta_num_col):
+    def _compare_grad_calculation(self, feat_gen, sample, per_target):
         """
         Check that the gradient calculation speed up is the same as the old basic gradient calculation
         """
-        per_target = theta_num_col == NUM_NUCLEOTIDES + 1
+        if per_target:
+            theta_num_col = NUM_NUCLEOTIDES + 1
+        else:
+            theta_num_col = 1
+
         theta = np.random.rand(feat_gen.feature_vec_len, theta_num_col)
         theta_mask = get_possible_motifs_to_targets(feat_gen.motif_list, theta.shape)
+        theta[~theta_mask] = -np.inf
+
         prob_solver = SurvivalProblemCustom(feat_gen, [sample], [1], per_target, theta_mask)
         sample_data = prob_solver.precalc_data[0]
 
@@ -45,19 +51,23 @@ class Survival_Problem_Gradient_Descent_TestCase(unittest.TestCase):
         )
         self.assertTrue(np.allclose(fast_grad, old_grad))
 
-    # @unittest.skip("skip")
     def test_grad_calculation(self):
-        self._compare_grad_calculation(self.feat_gen_hier, self.sample_hier, 1)
-        self._compare_grad_calculation(self.feat_gen_hier, self.sample_hier, NUM_NUCLEOTIDES)
+        self._compare_grad_calculation(self.feat_gen_hier, self.sample_hier, False)
+        self._compare_grad_calculation(self.feat_gen_hier, self.sample_hier, True)
 
-    def _compare_log_likelihood_calculation(self, feat_gen, sample, theta_num_col):
+    def _compare_log_likelihood_calculation(self, feat_gen, sample, per_target):
         """
         Check that the log likelihood calculation speed up is the same as the old basic log likelihood calculation
         """
-        per_target = theta_num_col == NUM_NUCLEOTIDES + 1
-        theta = np.random.rand(feat_gen.feature_vec_len, theta_num_col)
+        if per_target:
+            theta_num_col = NUM_NUCLEOTIDES + 1
+        else:
+            theta_num_col = 1
 
+        theta = np.random.rand(feat_gen.feature_vec_len, theta_num_col)
         theta_mask = get_possible_motifs_to_targets(feat_gen.motif_list, theta.shape)
+        theta[~theta_mask] = -np.inf
+
         prob_solver = SurvivalProblemCustom(feat_gen, [sample], [1], per_target, theta_mask)
         sample_data = prob_solver.precalc_data[0]
 
@@ -66,12 +76,12 @@ class Survival_Problem_Gradient_Descent_TestCase(unittest.TestCase):
         # Basic gradient calculation
         old_ll = self.calculate_log_likelihood_slow(theta, feat_gen, sample)
         # Fast log likelihood calculation
-        fast_ll = SurvivalProblemCustom.calculate_per_sample_log_lik(theta, sample_data)
+        fast_ll = SurvivalProblemCustom.calculate_per_sample_log_lik(theta, sample_data, per_target)
         self.assertTrue(np.allclose(fast_ll, old_ll))
 
     def test_log_likelihood_calculation(self):
-        self._compare_log_likelihood_calculation(self.feat_gen_hier, self.sample_hier, 1)
-        self._compare_log_likelihood_calculation(self.feat_gen_hier, self.sample_hier, NUM_NUCLEOTIDES)
+        self._compare_log_likelihood_calculation(self.feat_gen_hier, self.sample_hier, False)
+        self._compare_log_likelihood_calculation(self.feat_gen_hier, self.sample_hier, True)
 
     def calculate_grad_slow(self, theta, feat_gen, sample):
         per_target_model = theta.shape[1] == NUM_NUCLEOTIDES + 1
@@ -94,13 +104,20 @@ class Survival_Problem_Gradient_Descent_TestCase(unittest.TestCase):
                 grad[feature_dict[mutating_pos], col_idx] += 1
 
             grad_log_sum_exp = np.zeros(theta.shape)
-            denom = np.exp([
-                theta[f,0].sum() + theta[f,1:].sum(axis=0) for f in feature_dict.values()
-            ]).sum()
+            if per_target_model:
+                denom = np.exp([
+                    theta[f,0].sum() + theta[f,1:].sum(axis=0) for f in feature_dict.values()
+                ]).sum()
+            else:
+                denom = np.exp([theta[f,0].sum() for f in feature_dict.values()]).sum()
+
             denoms.append(denom)
             for f in feature_dict.values():
-                grad_log_sum_exp[f, 1:] = np.exp(theta[f, 0].sum() + theta[f, 1:].sum(axis=0))
-                grad_log_sum_exp[f, 0] = np.exp(theta[f, 0].sum() + theta[f, 1:].sum(axis=0)).sum()
+                if per_target_model:
+                    grad_log_sum_exp[f, 1:] += np.exp(theta[f, 0].sum() + theta[f, 1:].sum(axis=0))
+                    grad_log_sum_exp[f, 0] += np.exp(theta[f, 0].sum() + theta[f, 1:].sum(axis=0)).sum()
+                else:
+                    grad_log_sum_exp[f, 0] += np.exp(theta[f, 0].sum())
             grad -= grad_log_sum_exp/denom
 
             seq_str = mutate_string(
@@ -127,14 +144,19 @@ class Survival_Problem_Gradient_Descent_TestCase(unittest.TestCase):
             )
             # vecs_at_mutation_step[i] are the feature vectors of the at-risk group after mutation i
             feature_idx_mutated = feature_dict[mutating_pos]
-            denom = np.exp([theta[f,:].sum(axis=0) for f in feature_dict.values()]).sum()
+            if per_target_model:
+                denom = np.exp([
+                    theta[f,0].sum() + theta[f,1:].sum(axis=0) for f in feature_dict.values()
+                ]).sum()
+            else:
+                denom = np.exp([theta[f,0].sum() for f in feature_dict.values()]).sum()
             denoms.append(denom)
             log_denom = np.log(denom)
 
-            col_idx = 0
+            obj += theta[feature_idx_mutated, 0].sum() - log_denom
             if per_target_model:
-                col_idx = NUCLEOTIDE_DICT[sample.obs_seq_mutation.end_seq[mutating_pos]]
-            obj += theta[feature_idx_mutated, col_idx].sum() - log_denom
+                col_idx = NUCLEOTIDE_DICT[sample.obs_seq_mutation.end_seq[mutating_pos]] + 1
+                obj += theta[feature_idx_mutated, col_idx].sum()
 
             seq_str = mutate_string(
                 seq_str,