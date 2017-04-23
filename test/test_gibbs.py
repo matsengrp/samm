f071fd4068a1eea77e7d1fc14aba1cdfeeb1eef4
Jean Feng <jean.feng@gmail.com>
2017-04-23 16:56:53 -0700

    undo stash

--- test/test_gibbs.py
+++ test/test_gibbs.py
@@ -23,7 +23,6 @@ class Gibbs_TestCase(unittest.TestCase):
         cls.feat_gen_hier = HierarchicalMotifFeatureGenerator(motif_lens=[1,3])
         cls.obs = ObservedSequenceMutations("attcaaatgatatac", "ataaatagggtttac", cls.motif_len)
 
-<<<<<<< Updated upstream
     def _test_compute_log_probs(self, feat_gen, per_target_model):
         if per_target_model:
             theta = np.random.rand(feat_gen.feature_vec_len, NUM_NUCLEOTIDES + 1)
@@ -115,93 +114,6 @@ class Gibbs_TestCase(unittest.TestCase):
         for per_target_model in [False, True]:
             self._test_compute_log_probs_with_reference(self.feat_gen, per_target_model)
             self._test_compute_log_probs_with_reference(self.feat_gen_hier, per_target_model)
-=======
-    # def _test_compute_log_probs(self, feat_gen, per_target_model):
-    #     if per_target_model:
-    #         theta = np.random.rand(feat_gen.feature_vec_len, NUM_NUCLEOTIDES + 1)
-    #         possible_motif_mask = get_possible_motifs_to_targets(feat_gen.motif_list, theta.shape)
-    #         theta[~possible_motif_mask] = -np.inf
-    #     else:
-    #         theta = np.random.rand(feat_gen.feature_vec_len, 1) * 2
-    #     obs_seq_m = feat_gen.create_base_features(self.obs)
-    #     sampler = MutationOrderGibbsSampler(theta, feat_gen, obs_seq_m)
-    #
-    #     order = obs_seq_m.mutation_pos_dict.keys()
-    #
-    #     # This calculates denominators efficiently using deltas
-    #     feat_mut_steps, log_numerators, denominators = sampler._compute_log_probs_from_scratch(order)
-    #
-    #     self.assertEqual(len(log_numerators), len(order))
-    #     self.assertEqual(len(denominators), len(order))
-    #     seq_str = obs_seq_m.start_seq
-    #     for i in range(len(order)):
-    #         mutating_pos = order[i]
-    #         log_num = theta[feat_mut_steps[i].mutating_pos_feats, 0].sum()
-    #         if per_target_model:
-    #             col_idx = NUCLEOTIDE_DICT[obs_seq_m.end_seq[mutating_pos]] + 1
-    #             log_num += theta[feat_mut_steps[i].mutating_pos_feats, col_idx].sum()
-    #
-    #         feature_dict = feat_gen.create_for_sequence(
-    #             seq_str,
-    #             obs_seq_m.left_flank,
-    #             obs_seq_m.right_flank,
-    #             set(range(obs_seq_m.seq_len)) - set(order[:i])
-    #         )
-    #         # Calculates denominators from scratch - sum(exp(psi * theta))
-    #         denom = np.exp([
-    #             theta[feat_idx, 0].sum() + theta[feat_idx, 1:].sum(axis=0) for feat_idx in feature_dict.values()
-    #         ]).sum()
-    #         self.assertEqual(log_num, log_numerators[i])
-    #         self.assertTrue(np.isclose(denom, denominators[i]))
-    #
-    #         seq_str = mutate_string(
-    #             seq_str,
-    #             order[i],
-    #             obs_seq_m.end_seq[order[i]]
-    #         )
-    #
-    # def test_compute_log_probs(self):
-    #     # self._test_compute_log_probs(self.feat_gen_hier, False)
-    #     self._test_compute_log_probs(self.feat_gen_hier, True)
-    #
-    # def _test_compute_log_probs_with_reference(self, feat_gen, per_target_model):
-    #     obs_seq_m = feat_gen.create_base_features(self.obs)
-    #     if per_target_model:
-    #         num_cols = NUM_NUCLEOTIDES + 1
-    #     else:
-    #         num_cols = 1
-    #     theta = np.random.rand(feat_gen.feature_vec_len, num_cols) * 2
-    #     sampler = MutationOrderGibbsSampler(theta, feat_gen, obs_seq_m)
-    #
-    #     prev_order = obs_seq_m.mutation_pos_dict.keys()
-    #     curr_order = prev_order[:2] + [prev_order[3], prev_order[2]] + prev_order[4:]
-    #
-    #     prev_feat_mutation_steps, prev_log_numerators, prev_denominators = sampler._compute_log_probs_from_scratch(
-    #         prev_order,
-    #     )
-    #
-    #     _, curr_log_numerators, curr_denominators = sampler._compute_log_probs_from_scratch(
-    #         curr_order,
-    #     )
-    #
-    #     gibbs_step_info = GibbsStepInfo(
-    #         prev_order,
-    #         prev_log_numerators,
-    #         prev_denominators,
-    #     )
-    #     _, fast_log_numerators, fast_denominators = sampler._compute_log_probs_with_reference(
-    #         curr_order,
-    #         gibbs_step_info,
-    #         update_step_start=2,
-    #     )
-    #     self.assertTrue(np.allclose(curr_denominators, fast_denominators))
-    #     self.assertTrue(np.allclose(curr_log_numerators, fast_log_numerators))
-    #
-    # def test_compute_log_probs_with_reference(self):
-    #     for per_target_model in [False, True]:
-    #         self._test_compute_log_probs_with_reference(self.feat_gen, per_target_model)
-    #         self._test_compute_log_probs_with_reference(self.feat_gen_hier, per_target_model)
->>>>>>> Stashed changes
 
     def _test_joint_distribution(self, feat_gen, theta):
         """
@@ -266,7 +178,6 @@ class Gibbs_TestCase(unittest.TestCase):
         for t, g in zip(true_counter.most_common(NUM_TOP_COMMON), gibbs_counter.most_common(NUM_TOP_COMMON)):
             print "%s (%d) \t %s (%d)" % (t[0], t[1], g[0], g[1])
         return rho, pval
-<<<<<<< Updated upstream
 
     @unittest.skip("asdf")
     def test_joint_distribution_simple(self):
@@ -282,22 +193,6 @@ class Gibbs_TestCase(unittest.TestCase):
         rho, pval = self._test_joint_distribution(self.feat_gen_hier, theta)
         self.assertTrue(rho > 0.95)
         self.assertTrue(pval < 1e-33)
-=======
-    #
-    # def test_joint_distribution_simple(self):
-    #     """
-    #     Test the joint distributions match for a single column theta (not a per-target-nucleotide model)
-    #     """
-    #     theta = np.random.rand(self.feat_gen.feature_vec_len, 1) * 2
-    #     rho, pval = self._test_joint_distribution(self.feat_gen, theta)
-    #     self.assertTrue(rho > 0.95)
-    #     self.assertTrue(pval < 1e-33)
-    #
-    #     theta = np.random.rand(self.feat_gen_hier.feature_vec_len, 1)
-    #     rho, pval = self._test_joint_distribution(self.feat_gen_hier, theta)
-    #     self.assertTrue(rho > 0.95)
-    #     self.assertTrue(pval < 1e-33)
->>>>>>> Stashed changes
 
     def test_joint_distribution_per_target_model(self):
         """
