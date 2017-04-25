c890139d8297adf6bd98e4fcde291c9d8ac99c64
Jean Feng <jean.feng@gmail.com>
2017-04-25 10:21:59 -0700

    remove conf int from pkl

--- confidence_interval_maker.py
+++ confidence_interval_maker.py
@@ -19,6 +19,8 @@ class ConfidenceIntervalMaker:
         since theta is an MLE.
 
         @param z: the z-statistic that controls the width of the confidence intervals
+
+        @return standard error estimates for the theta parameters
         """
         sample_obs_information, _ = problem.get_hessian(theta)
         # Need to filter out all the theta values that are constant (negative infinity or zero constants)
@@ -34,14 +36,14 @@ class ConfidenceIntervalMaker:
                 conf_ints = self._create_confidence_intervals(standard_errors, theta, z)
                 log.info("Confidence Interval Estimates:")
                 log.info(self._get_confidence_interval_print_lines(conf_ints))
-                return standard_errors, conf_ints
+                return standard_errors
             else:
                 log.info("Confidence interval: variance estimates are negative")
                 log.info("variance %s" % np.diag(variance_est))
         else:
             log.info("Confidence interval: observation matrix is singular")
             log.info("np.linalg.eigvals %s" % np.linalg.eigvals(sample_obs_information))
-        return None, None
+        return None
 
     def _create_confidence_intervals(self, standard_errors, theta, z=1.96):
         """
