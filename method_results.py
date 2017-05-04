8d40880b237bb56c2559a55fecebfec955a9e642
Jean Feng <jean.feng@gmail.com>
2017-05-03 20:09:46 -0700

    store variance matrix instead

--- method_results.py
+++ method_results.py
@@ -2,11 +2,11 @@ import numpy as np
 from common import *
 
 class MethodResults:
-    def __init__(self, penalty_params, theta, fitted_prob_vector, theta_standard_error=None):
+    def __init__(self, penalty_params, theta, fitted_prob_vector, variance_est=None):
         self.penalty_params = penalty_params
         self.theta = theta
         self.fitted_prob_vector = fitted_prob_vector
-        self.theta_standard_error = theta_standard_error
+        self.variance_est = variance_est
         self.num_nonzero = get_num_nonzero(self.theta)
         self.num_unique = get_num_unique_theta(self.theta)
 
