3d4f89f8e307a7eb7509958e10d0be21ca5b1127
D. Shaw <wahsadivad@gmail.com>
2017-05-10 17:22:18 -0700

    work on hierarchical and offset motifs

--- create_bar_plots.py
+++ create_bar_plots.py
@@ -9,7 +9,6 @@ import pickle
 import csv
 import itertools
 
-from itertools import izip
 from hier_motif_feature_generator import HierarchicalMotifFeatureGenerator
 from read_data import read_zero_motif_csv
 from common import *
@@ -59,17 +58,18 @@ def parse_args():
 
     return args
 
-def convert_to_csv(target, theta_vals, motif_list, theta_lower, theta_upper, motif_lens, mutating_pos_list, feat_generator, num_mut_pos):
+def convert_to_csv(output_csv, theta_vals, theta_lower, theta_upper, full_feat_generator):
     """
     Take pickle file and convert to csv for use in R
     """
-    padded_list = list(motif_list* num_mut_pos)
+    padded_list = list(full_feat_generator.motif_list)
 
-    if num_mut_pos > 1:
+    if full_feat_generator.num_feat_gens > 1:
         # pad offset motifs with Ns
-        for idx, (motif, mut_pos) in enumerate(zip(padded_list, mutating_pos_list)):
-            left_pad = (feat_generator.max_left_motif_flank_len - mut_pos) * 'n'
-            right_pad = (mut_pos - feat_generator.max_right_motif_flank_len + feat_generator.motif_len - 1) * 'n'
+        # if center base is mutating this will just yield usual motif list
+        for idx, (motif, mut_pos) in enumerate(zip(padded_list, full_feat_generator.mutating_pos_list)):
+            left_pad = (full_feat_generator.max_left_motif_flank_len - mut_pos) * 'n'
+            right_pad = (mut_pos - full_feat_generator.max_right_motif_flank_len + full_feat_generator.motif_len - 1) * 'n'
             padded_list[idx] = left_pad + motif + right_pad
 
     header = ['motif', 'target', 'theta', 'theta_lower', 'theta_upper']
@@ -78,126 +78,77 @@ def convert_to_csv(target, theta_vals, motif_list, theta_lower, theta_upper, mot
         for motif, tval, tlow, tup in zip(padded_list, theta_vals[:, col_idx], theta_lower[:, col_idx], theta_upper[:, col_idx]):
             data.append([motif.upper(), col_idx, tval, tlow, tup])
 
-    with open(str(target), 'wb') as f:
+    with open(str(output_csv), 'wb') as f:
         writer = csv.writer(f)
         writer.writerow(header)
         writer.writerows(data)
 
-def get_theta_conf_int(args, feat_generator, full_feat_generator, theta, covariance_est, col_idx, num_mut_pos):
-    full_theta_size = (4**args.max_motif_len) * (num_mut_pos)
+def combine_thetas_and_get_conf_int(feat_generator, full_feat_generator, theta, covariance_est, col_idx):
+    """
+    Combine hierarchical and offset theta values
+    """
+    full_theta_size = full_feat_generator.feature_vec_len
     full_theta = np.zeros(full_theta_size)
     theta_lower = np.zeros(full_theta_size)
     theta_upper = np.zeros(full_theta_size)
 
-    if len(args.motif_len_vals) > 1 and num_mut_pos == 1:
-        # Combine the hierarchical thetas if that is the case
-        theta_index_matches = {i:[] for i in range(full_theta_size)}
+    theta_index_matches = {i:[] for i in range(full_theta_size)}
 
-        start_idx = 0
-        for feat_gen in feat_generator.feat_gens[:len(args.motif_len_vals)]:
-            motif_list = feat_gen.motif_list
-            for m_idx, m in enumerate(motif_list):
+    for i, feat_gen in enumerate(feat_generator.feat_gens):
+        for m_idx, m in enumerate(feat_gen.motif_list):
+            raw_theta_idx = feat_generator.feat_offsets[i] + m_idx
+            m_theta = theta[raw_theta_idx, 0]
 
-                raw_theta_idx = start_idx + m_idx
-                m_theta = theta[raw_theta_idx, 0]
+            if col_idx != 0:
+                m_theta += theta[raw_theta_idx, col_idx]
+
+            if feat_gen.motif_len == full_feat_generator.motif_len:
+                # Already at maximum motif length, so nothing to combine
+                full_m_idx = full_feat_generator.motif_dict[m][feat_gen.left_motif_flank_len]
+                full_theta[full_m_idx] += m_theta
+
+                theta_index_matches[full_m_idx].append(raw_theta_idx)
                 if col_idx != 0:
-                    m_theta += theta[raw_theta_idx, col_idx]
-
-                if feat_gen.hier_offset == 0:
-                    full_m_idx = full_feat_generator.motif_dict[m]
-                    full_theta[full_m_idx] += m_theta
-
-                    theta_index_matches[full_m_idx].append(raw_theta_idx)
-                    if col_idx != 0:
-                        theta_index_matches[full_m_idx].append(raw_theta_idx + col_idx * theta.shape[0])
-                else:
-                    flanks = itertools.product(["a", "c", "g", "t"], repeat=2*feat_gen.hier_offset)
+                    theta_index_matches[full_m_idx].append(raw_theta_idx + col_idx * theta.shape[0])
+            else:
+                # Combine hierarchical feat_gens for given left_motif_len
+                for full_feat_gen in full_feat_generator.feat_gens:
+                    flanks = itertools.product(["a", "c", "g", "t"], repeat=full_feat_gen.motif_len - feat_gen.motif_len)
                     for f in flanks:
-                        # assume for now hierarchical will just have center mutating
                         full_m = "".join(f[:feat_gen.hier_offset]) + m + "".join(f[feat_gen.hier_offset:])
-                        full_m_idx = full_feat_generator.motif_dict[full_m]
+                        full_m_idx = full_feat_generator.motif_dict[full_m][full_feat_gen.left_motif_flank_len]
                         full_theta[full_m_idx] += m_theta
 
                         theta_index_matches[full_m_idx].append(raw_theta_idx)
                         if col_idx != 0:
                             theta_index_matches[full_m_idx].append(raw_theta_idx + col_idx * theta.shape[0])
 
-            start_idx += len(motif_list)
+    for full_theta_idx, matches in theta_index_matches.iteritems():
+        var_est = 0
+        for i in matches:
+            for j in matches:
+                var_est += covariance_est[i,j]
 
-        for full_theta_idx, matches in theta_index_matches.iteritems():
-            var_est = 0
-            for i in matches:
-                for j in matches:
-                    var_est += covariance_est[i,j]
-
-            standard_err_est = np.sqrt(var_est)
-            theta_lower[full_theta_idx] = full_theta[full_theta_idx] - ZSCORE_95 * standard_err_est
-            theta_upper[full_theta_idx] = full_theta[full_theta_idx] + ZSCORE_95 * standard_err_est
-
-    elif num_mut_pos > 1 and len(args.motif_len_vals) == 1:
-        # Combine the hierarchical thetas if that is the case
-        theta_index_matches = {i:[] for i in range(full_theta_size)}
-
-        start_idx = 0
-        for feat_gen in feat_generator.feat_gens[:num_mut_pos]:
-            motif_list = feat_gen.motif_list
-            for m_idx, m in enumerate(motif_list):
-
-                raw_theta_idx = start_idx + m_idx
-                m_theta = theta[raw_theta_idx, 0]
-                if col_idx != 0:
-                    if np.isfinite(theta[raw_theta_idx, col_idx]):
-                        m_theta += theta[raw_theta_idx, col_idx]
-                    else:
-                        m_theta = 0
-
-                full_m_idx = start_idx + full_feat_generator.motif_dict[m]
-                full_theta[full_m_idx] += m_theta
-
-                theta_index_matches[full_m_idx].append(raw_theta_idx)
-                if col_idx != 0:
-                    theta_index_matches[full_m_idx].append(raw_theta_idx + col_idx * theta.shape[0])
-
-            start_idx += len(motif_list)
-
-        for full_theta_idx, matches in theta_index_matches.iteritems():
-            var_est = 0
-            for i in matches:
-                for j in matches:
-                    var_est += covariance_est[i,j]
-
-            standard_err_est = np.sqrt(var_est)
-            theta_lower[full_theta_idx] = full_theta[full_theta_idx] - ZSCORE_95 * standard_err_est
-            theta_upper[full_theta_idx] = full_theta[full_theta_idx] + ZSCORE_95 * standard_err_est
-    else:
-        for i, m in enumerate(full_feat_generator.motif_list):
-            if m in feat_generator.motif_dict:
-                theta_idx = feat_generator.motif_dict[m]
-                full_theta[i] = theta[theta_idx]
-                standard_err_est = np.sqrt(covariance_est[theta_idx, theta_idx])
-                theta_lower[i] = theta[theta_idx] - ZSCORE_95 * standard_err_est
-                theta_upper[i] = theta[theta_idx] + ZSCORE_95 * standard_err_est
+        standard_err_est = np.sqrt(var_est)
+        theta_lower[full_theta_idx] = full_theta[full_theta_idx] - ZSCORE_95 * standard_err_est
+        theta_upper[full_theta_idx] = full_theta[full_theta_idx] + ZSCORE_95 * standard_err_est
 
     return full_theta, theta_lower, theta_upper
 
-def plot_theta(args, full_theta, motif_list, theta_lower, theta_upper, output_pdf, mutating_pos_list, targets, feat_generator, num_mut_pos):
+def plot_theta(output_csv, full_theta, theta_lower, theta_upper, output_pdf, targets, full_feat_generator, max_motif_len):
     convert_to_csv(
-        args.output_csv,
+        output_csv,
         full_theta,
-        motif_list,
         theta_lower,
         theta_upper,
-        [args.max_motif_len],
-        mutating_pos_list,
-        feat_generator,
-        num_mut_pos
+        full_feat_generator,
     )
 
     # Call Rscript
     command = 'Rscript'
     script_file = 'R/create_bar_plot_from_file.R'
 
-    cmd = [command, script_file, args.output_csv, str(args.max_motif_len), output_pdf, targets]
+    cmd = [command, script_file, output_csv, str(max_motif_len), output_pdf, targets]
     print "Calling:", " ".join(cmd)
     res = subprocess.call(cmd)
 
@@ -209,33 +160,32 @@ def main(args=sys.argv[1:]):
     for m in args.motif_len_vals:
         assert(m % 2 == 1)
         
+    args.max_motif_len = max(args.motif_len_vals)
+
     if args.positions_mutating is None:
         # default to central base mutating
-        args.max_left_flank = None
-        args.max_right_flank = None
         args.positions_mutating = [[m/2] for m in args.motif_len_vals]
+        args.max_mut_pos = [[args.max_motif_len/2]]
     else:
         args.positions_mutating = [[int(m) for m in positions.split(',')] for positions in args.positions_mutating.split(':')]
         for motif_len, positions in zip(args.motif_len_vals, args.positions_mutating):
             for m in positions:
                 assert(m in range(motif_len))
+        args.max_mut_pos = [mut_pos for mut_pos, motif_len in zip(args.positions_mutating, args.motif_len_vals) if motif_len == args.max_motif_len]
 
-    args.max_motif_len = max(args.motif_len_vals)
-
-    motifs_to_remove, target_pairs_to_remove = read_zero_motif_csv(args.zero_motifs, args.per_target_model)
+    motifs_to_remove, pos_to_remove, target_pairs_to_remove = read_zero_motif_csv(args.zero_motifs, args.per_target_model)
     feat_generator = HierarchicalMotifFeatureGenerator(
         motif_lens=args.motif_len_vals,
         motifs_to_remove=motifs_to_remove,
+        pos_to_remove=pos_to_remove,
         left_motif_flank_len_list=args.positions_mutating,
     )
     mutating_pos_list = feat_generator.mutating_pos_list
-    # number of offsets
-    num_mut_pos = feat_generator.num_feat_gens / len(args.motif_len_vals)
 
     full_feat_generator = HierarchicalMotifFeatureGenerator(
         motif_lens=[args.max_motif_len],
+        left_motif_flank_len_list=args.max_mut_pos,
     )
-    motif_list = full_feat_generator.motif_list
 
     # Load fitted theta file
     with open(args.input_pkl, "r") as f:
@@ -261,30 +211,23 @@ def main(args=sys.argv[1:]):
     #     sample_obs_information = (fisher_info1[theta_mask_flat,:])[:,theta_mask_flat]
     #     covariance_est = np.linalg.inv(sample_obs_information)
 
-    if len(args.motif_len_vals) > 1:
-        # hierarchical model combines values
-        full_theta = np.zeros((len(motif_list), theta.shape[1]))
-        theta_lower = np.zeros((len(motif_list), theta.shape[1]))
-        theta_upper = np.zeros((len(motif_list), theta.shape[1]))
-    else:
-        # otherwise just use shape of theta
-        full_theta = np.zeros(theta.shape)
-        theta_lower = np.zeros(theta.shape)
-        theta_upper = np.zeros(theta.shape)
+    full_theta = np.zeros((full_feat_generator.feature_vec_len, theta.shape[1]))
+    theta_lower = np.zeros((full_feat_generator.feature_vec_len, theta.shape[1]))
+    theta_upper = np.zeros((full_feat_generator.feature_vec_len, theta.shape[1]))
 
     for col_idx in range(theta.shape[1]):
         full_theta[:,col_idx], theta_lower[:,col_idx], theta_upper[:,col_idx] = \
-                get_theta_conf_int(args, feat_generator, full_feat_generator, theta, covariance_est, col_idx, num_mut_pos)
+                combine_thetas_and_get_conf_int(feat_generator, full_feat_generator, theta, covariance_est, col_idx)
 
     if args.per_target_model:
         if args.plot_separate:
             for col_idx, target in enumerate(['N', 'A', 'C', 'G', 'T']):
                 output_pdf = args.output_pdf.replace(".pdf", "_col%d.pdf" % col_idx)
-                plot_theta(args, full_theta, motif_list, theta_lower, theta_upper, output_pdf, mutating_pos_list, target, feat_generator, num_mut_pos)
+                plot_theta(args.output_csv, full_theta, theta_lower, theta_upper, output_pdf, target, full_feat_generator, args.max_motif_len)
         else:
-            plot_theta(args, full_theta, motif_list, theta_lower, theta_upper, args.output_pdf, mutating_pos_list, 'N,A,C,G,T', feat_generator, num_mut_pos)
+            plot_theta(args.output_csv, full_theta, theta_lower, theta_upper, args.output_pdf, 'N,A,C,G,T', full_feat_generator, args.max_motif_len)
     else:
-        plot_theta(args, full_theta, motif_list, theta_lower, theta_upper, args.output_pdf, mutating_pos_list, 'N', feat_generator, num_mut_pos)
+        plot_theta(args.output_csv, full_theta, theta_lower, theta_upper, args.output_pdf, 'N', full_feat_generator, args.max_motif_len)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
