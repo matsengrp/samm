b64b6b8b8ea1069663986e500420bd6d4a427034
Jean Feng <jean.feng@gmail.com>
2017-05-27 13:33:55 -0700

    add min l1 norm theta

--- plot_simulation_section.py
+++ plot_simulation_section.py
@@ -6,6 +6,7 @@ import scipy.stats
 
 from hier_motif_feature_generator import HierarchicalMotifFeatureGenerator
 from confidence_interval_maker import ConfidenceIntervalMaker
+from make_model_sparse import SparseModelMaker
 from common import *
 
 def parse_args():
@@ -28,7 +29,7 @@ def parse_args():
     parser.add_argument('--stat',
         type=str,
         default="norm",
-        choices=("norm", "coverage", "support", "pearson"),
+        choices=("norm", "raw_norm", "coverage", "raw_coverage", "raw_pearson", "pearson", "support"),
     )
     parser.add_argument('--z',
         type=float,
@@ -43,10 +44,18 @@ def parse_args():
 
     if args.stat == "norm":
         args.stat_func = _get_agg_norm_diff
+    elif args.stat == "raw_norm":
+        args.stat_func = _get_raw_norm_diff
+    elif args.stat == "raw_coverage":
+        args.stat_func = _get_raw_coverage
     elif args.stat == "coverage":
         args.stat_func = _get_agg_coverage
+    elif args.stat == "raw_pearson":
+        args.stat_func = _get_raw_pearson
     elif args.stat == "pearson":
-        args.stat_func = _get_pearson
+        args.stat_func = _get_agg_pearson
+    elif args.stat == "support":
+        args.stat_func = _get_support
 
     return args
 
@@ -89,17 +98,31 @@ def _collect_statistics(fitted_models, args, raw_true_theta, agg_true_theta, sta
     statistics = [stat_func(fmodel, dense_agg_feat_gen, raw_true_theta, agg_true_theta, possible_agg_mask) for fmodel in fitted_models if fmodel is not None]
     return statistics
 
-def _get_pearson(fmodel, full_feat_generator, raw_true_theta, agg_true_theta, possible_agg_mask):
+def _get_support(fmodel, full_feat_generator, raw_true_theta, agg_true_theta, possible_agg_mask):
+    raw_refit_theta = np.zeros(raw_true_theta.shape)
+    raw_refit_theta[~fmodel.model_masks.feats_to_remove_mask, :] = fmodel.refit_theta
+    support_refit_theta = (raw_refit_theta != -np.inf) & (raw_refit_theta != 0)
+    true_support = (raw_true_theta != -np.inf) & (raw_true_theta != 0)
+    return float(np.sum(support_refit_theta & true_support))/np.sum(support_refit_theta)
+
+def _get_raw_pearson(fmodel, full_feat_generator, raw_true_theta, agg_true_theta, possible_agg_mask):
+    raw_refit_theta = np.zeros(raw_true_theta.shape)
+    raw_refit_theta[~fmodel.model_masks.feats_to_remove_mask, :] = fmodel.refit_theta
+    possible_raw_mask = raw_true_theta != -np.inf
+    return scipy.stats.pearsonr(raw_true_theta[possible_raw_mask], raw_refit_theta[possible_raw_mask])[0]
+
+def _get_agg_pearson(fmodel, full_feat_generator, raw_true_theta, agg_true_theta, possible_agg_mask):
     return scipy.stats.pearsonr(
         agg_true_theta[possible_agg_mask],
         fmodel.agg_refit_theta[possible_agg_mask],
     )[0]
 
-def _get_raw_pearson(fmodel, full_feat_generator, raw_true_theta, agg_true_theta, possible_agg_mask):
-    return scipy.stats.pearsonr(
-        raw_true_theta[~fmodel.model_masks.feats_to_remove_mask][fmodel.refit_theta != -np.inf],
-        fmodel.refit_theta[fmodel.refit_theta != -np.inf],
-    )[0]
+def _get_raw_norm_diff(fmodel, full_feat_generator, raw_true_theta, agg_true_theta, possible_agg_mask):
+    raw_refit_theta = np.zeros(raw_true_theta.shape)
+    raw_refit_theta[~fmodel.model_masks.feats_to_remove_mask, :] = fmodel.refit_theta
+    possible_raw_mask = raw_true_theta != -np.inf
+    tot_elems = np.sum(possible_raw_mask)
+    return np.linalg.norm(raw_true_theta[possible_raw_mask] - raw_refit_theta[possible_raw_mask])/np.sqrt(tot_elems)
 
 def _get_agg_norm_diff(fmodel, full_feat_generator, raw_true_theta, agg_true_theta, possible_agg_mask):
     tot_elems = np.sum(possible_agg_mask)
@@ -147,11 +170,11 @@ def _get_agg_coverage(fmodel, full_feat_generator, raw_true_theta, agg_true_thet
         agg_fitted_lower -= med_theta
         agg_fitted_upper -= med_theta
         comparison_mask = np.abs(agg_fitted_lower - agg_fitted_upper) > 1e-5 # only look at things with confidence intervals
-        print np.hstack((
-            agg_fitted_lower.reshape((agg_fitted_lower.size,1)),
-            agg_true_theta[:, col_idx:col_idx + 1] - np.median(agg_true_theta[agg_true_theta != -np.inf]),
-            agg_fitted_upper.reshape((agg_fitted_upper.size,1))
-        ))
+        # print np.hstack((
+        #     agg_fitted_lower.reshape((agg_fitted_lower.size,1)),
+        #     agg_true_theta[:, col_idx:col_idx + 1] - np.median(agg_true_theta[agg_true_theta != -np.inf]),
+        #     agg_fitted_upper.reshape((agg_fitted_upper.size,1))
+        # ))
         num_considered = np.sum(comparison_mask)
         tot_considered += num_considered
         # comparison_mask = np.ones(agg_fitted_lower.shape, dtype=bool)
@@ -166,32 +189,67 @@ def _get_agg_coverage(fmodel, full_feat_generator, raw_true_theta, agg_true_thet
 
 def _get_raw_coverage(fmodel, full_feat_generator, raw_true_theta, true_theta, possible_agg_mask):
     conf_int = ConfidenceIntervalMaker.create_confidence_intervals(
-        fmodel.refit_theta - np.median(fmodel.refit_theta),
+        fmodel.refit_theta,
         np.sqrt(np.diag(fmodel.variance_est)),
         fmodel.refit_possible_theta_mask,
         fmodel.model_masks.zero_theta_mask_refit,
-        z=1.96,
+        z=ZSCORE_95,
     )
 
-    raw_true_theta -= np.median(raw_true_theta)
-    true_theta_trunc = raw_true_theta[~fmodel.model_masks.feats_to_remove_mask]
+    theta_mask = fmodel.refit_possible_theta_mask & ~fmodel.model_masks.zero_theta_mask_refit
+    theta_mask_flat = theta_mask.reshape((theta_mask.size,), order="F")
+
+    true_theta_trunc = np.array(raw_true_theta[~fmodel.model_masks.feats_to_remove_mask, :])
     true_theta_trunc = true_theta_trunc.reshape((true_theta_trunc.size,), order="F")
+    true_theta_trunc = true_theta_trunc[theta_mask_flat]
+    print np.hstack((
+        conf_int[:,0:1],
+        true_theta_trunc.reshape((true_theta_trunc.size, 1)),
+        conf_int[:,2:3]
+    ))
     return np.mean((conf_int[:,0] <= true_theta_trunc) & (true_theta_trunc <= conf_int[:,2]))
 
-
-def _load_true_model(file_name):
+def _load_true_model(file_name, agg_motif_len, agg_pos_mutating, hier_motif_lens, hier_positions_mutating):
     with open(file_name, "r") as f:
         true_model_agg, true_model = pickle.load(f)
-    return true_model_agg, true_model
+
+    dense_agg_feat_gen = HierarchicalMotifFeatureGenerator(
+        motif_lens=[agg_motif_len],
+        left_motif_flank_len_list=[[agg_pos_mutating]],
+    )
+
+    dense_hier_feat_gen = HierarchicalMotifFeatureGenerator(
+        motif_lens=hier_motif_lens,
+        left_motif_flank_len_list=hier_positions_mutating,
+    )
+
+    sparse_raw_theta = SparseModelMaker.solve(true_model_agg, dense_hier_feat_gen, dense_agg_feat_gen, raw_theta=true_model)
+    print "sparse_raw_theta v true", np.hstack((sparse_raw_theta, true_model))
+    print "l1 min soln", np.sum(sparse_raw_theta == 0)
+    print "l1 l1", np.linalg.norm(sparse_raw_theta, 1)
+    print "true model", np.sum(true_model == 0)
+    print "l1 true_model", np.linalg.norm(true_model[true_model != -np.inf], 1)
+    possible_raw_theta_mask = get_possible_motifs_to_targets(dense_hier_feat_gen.motif_list, sparse_raw_theta.shape, dense_hier_feat_gen.mutating_pos_list)
+    sparse_raw_theta[~possible_raw_theta_mask] = -np.inf
+    return np.array(true_model_agg), np.array(sparse_raw_theta)
 
 def main(args=sys.argv[1:]):
     args = parse_args()
 
-    true_thetas = [_load_true_model(tmodel) for tmodel in args.true_models]
-
     fitted_models = [
         [load_fitted_model(file_name, args.agg_motif_len, args.agg_pos_mutating) for file_name in fnames]
         for fnames in args.fitted_models
+    ]
+    example_model = fitted_models[0][0]
+
+    true_thetas = [
+        _load_true_model(
+            tmodel,
+            args.agg_motif_len,
+            args.agg_pos_mutating,
+            example_model.motif_lens,
+            example_model.positions_mutating,
+        ) for tmodel in args.true_models
     ]
 
     num_cols = fitted_models[0][0].refit_theta.shape[1]
