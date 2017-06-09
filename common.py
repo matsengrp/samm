72fd12ee6427c6a905ca4f54143f6e98d1951e1a
D. Shaw <wahsadivad@gmail.com>
2017-06-09 11:58:43 -0700

    update data run parameters

--- common.py
+++ common.py
@@ -624,7 +624,7 @@ def pick_best_model(fitted_models):
     """
     Select the one with largest (pseudo) log lik ratio
     """
-    good_models = [f_model for f_model in fitted_models if f_model.has_refit_data and f_model.variance_est is not None]
+    good_models = [f_model for f_model in fitted_models if f_model.has_refit_data]
     for max_idx in reversed(range(len(good_models))):
         if good_models[max_idx].log_lik_ratio_lower_bound > 0:
             break
