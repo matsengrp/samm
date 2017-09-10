c547a72eecf11d217e6a061dd6a08d107c50e93a
Jean Feng <jean.feng@gmail.com>
2017-09-10 16:47:14 -0700

    fix simulation shmulate

--- R/shmulate_sequences.R
+++ R/shmulate_sequences.R
@@ -17,7 +17,7 @@ run_shmulate <- function(germline) {
     # of sequence length.
     seq_len <- nchar(germline)
     num_muts <- floor(runif(1, min=min_pct_mut * seq_len, max =max_pct_mut * seq_len))
-    tolower(shmulateSeq(toupper(germline), num_muts))
+    tolower(shmulateSeq(toupper(germline), num_muts, targetingModel = MK_RS5NF))
 }
 
 genes <- read.csv(gene_file, stringsAsFactors=FALSE)
@@ -32,20 +32,15 @@ genes <- merge(genes, taxa_per_genes, by="germline_name")
 seqs <- apply(genes, 1, function(gene_data) {
   gene_name <- gene_data[1]
   gene_seq <- gene_data[2]
-  gene_freq <- as.numeric(gene_data[3])
   n_germ_taxa <- as.numeric(gene_data[5])
 
   mutated_seqs <- replicate(n_germ_taxa, run_shmulate(gene_seq))
   seq_names <- paste0(gene_name, '-', seq(n_germ_taxa))
   if (n_germ_taxa > 0) {
     data.frame(
-      germline_name=gene_name,
-      locus='',
-      species='',
+      germline_name=rep(gene_name, n_germ_taxa),
       # each sequence is one clonal family
       clonal_family=seq_names,
-      group='',
-      subject='',
       sequence_name=seq_names,
       sequence=mutated_seqs
     )
@@ -53,7 +48,7 @@ seqs <- apply(genes, 1, function(gene_data) {
     data.frame()
   }
 })
-seq_data_frame <- do.call("rbind", seqs)
+seq_data_frame <- do.call("rbind", seqs[lengths(seqs) > 0])
 
 write.csv(
     seq_data_frame,
