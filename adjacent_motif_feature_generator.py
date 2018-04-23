573e0d7d727a5fd9b19ac2d325ae12355ee4ea60
D. Shaw <wahsadivad@gmail.com>
2018-04-23 13:26:27 -0700

    gah forgot update region

--- adjacent_motif_feature_generator.py
+++ adjacent_motif_feature_generator.py
@@ -14,13 +14,23 @@ class AdjacentMotifFeatureGenerator(GenericFeatureGenerator):
             motif_len=4,
             distance_to_right_flank_end=0,
             feats_to_remove=[],
+            left_update_region=1,
+            right_update_region=1
         ):
         """
         @param motif_len: list of motifs to look for at a distance from mutating position
         @param distance_to_right_flank_end: distance from mutating position to look for nearby motif
+        @param feats_to_remove: motifs to remove
+        @param left_update_region: number of positions to consider left of mutating position to update
+            features correctly
+        @param right_update_region: number of positions to consider right of mutating position to update
+            features correctly
         """
         self.motif_len = motif_len
         self.distance_to_right_flank_end = distance_to_right_flank_end
+        self.left_update_region = left_update_region
+        self.right_update_region = right_update_region
+
         self.update_feats_after_removing(feats_to_remove)
 
     def update_feats_after_removing(self, feats_to_remove=[]):
@@ -57,8 +67,8 @@ class AdjacentMotifFeatureGenerator(GenericFeatureGenerator):
         @return a dict with the positions next to the given position and their feature index
         """
         feat_dict = dict()
-        start_region_idx = max(position + self.distance_to_right_flank_end - self.motif_len, 0)
-        end_region_idx = min(position + self.distance_to_right_flank_end, seq_len)
+        start_region_idx = max(position - self.left_update_region, 0)
+        end_region_idx = min(position + self.right_update_region, seq_len - 1)
         update_positions = range(start_region_idx, position) + range(position + 1, end_region_idx)
         for pos in update_positions:
             if pos not in already_mutated_pos:
