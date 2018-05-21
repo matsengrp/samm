a730c7023d9a2848c124554caa4704923921e309
Jean Feng <jean.feng@gmail.com>
2018-05-21 13:05:25 -0700

    add tuning penalty params for logistic

--- fit_logistic_model.py
+++ fit_logistic_model.py
@@ -1,7 +1,7 @@
 """
 We fit a logistic regression model to understand mutation rates instead.
 """
-
+import copy
 import sys
 import argparse
 import os
@@ -9,12 +9,13 @@ import os.path
 import logging as log
 from sklearn.linear_model import LogisticRegressionCV
 import scipy.sparse
-
 import numpy as np
 
+import data_split
 from hier_motif_feature_generator import HierarchicalMotifFeatureGenerator
 from logistic_problem_cvxpy import LogisticRegressionMotif
 from fit_model_common import process_motif_length_args
+from model_truncation import ModelTruncation
 from common import *
 from read_data import *
 
@@ -53,14 +54,21 @@ def parse_args():
         and 5mer with first, second and third
         """,
         default=None)
-    parser.add_argument('--min-C',
+    parser.add_argument('--penalty-params',
+        type=str,
+        help="Comma-separated list of penalty parameters",
+        default="8.0, 4.0, 2.0, 1.0, 0.5, 0.25")
+    parser.add_argument('--tuning-sample-ratio',
         type=float,
-        help='Minimum C (inverse of penalty param) for logistic regression in sckitlearn',
-        default=0)
-    parser.add_argument('--max-C',
-        type=float,
-        help='Maximum C for logistic regression in sckitlearn',
-        default=0.4)
+        help="Proportion of data to reserve for tuning the penalty parameter",
+        default=0.1)
+    parser.add_argument('--k-folds',
+        type=int,
+        help="num folds for CV",
+        default=1)
+    parser.add_argument('--validation-col',
+        type=str,
+        default=None)
     parser.add_argument('--per-target-model',
         action='store_true',
         help='Fit a model that allows for different hazard rates for different target nucleotides')
@@ -82,8 +90,54 @@ def parse_args():
         args.theta_num_col = 1
 
     process_motif_length_args(args)
+    args.penalty_params = [float(p) for p in args.penalty_params.split(",")]
+    args.penalty_params = sorted(args.penalty_params, reverse=True)
 
     return args
+
+def get_X_y_matrices(obs_data, per_target_model):
+    # Process data
+    X = []
+    ys = []
+    for i, obs in enumerate(obs_data):
+        X.append(obs.feat_matrix_start)
+        if per_target_model:
+            y_vec = np.zeros(obs.seq_len)
+            for k, v in obs.mutation_pos_dict.iteritems():
+                y_vec[k] = NUCLEOTIDE_DICT[v] + 1
+            ys.append(y_vec)
+        else:
+            ys.append(np.array(obs.mutated_indicator))
+
+    stacked_X = scipy.sparse.vstack(X).todense()
+    stacked_y = np.concatenate(ys)
+    return stacked_X, stacked_y
+
+def get_best_penalty_param(penalty_params, data_folds):
+    # Fit the models for each penalty parameter
+    tot_validation_values = []
+    for penalty_param in penalty_params:
+        tot_validation_value = 0
+        for fold_idx, (val_X, val_y, logistic_reg) in enumerate(data_folds):
+            theta, train_value = logistic_reg.solve(lam_val=penalty_param, max_iters=9000)
+            tot_validation_value += logistic_reg.score(val_X, val_y)
+            log.info("theta support %d", np.sum(np.abs(theta) > 1e-5))
+        tot_validation_values.append([penalty_param, tot_validation_value])
+        log.info("penalty_param %f, tot_val %f", penalty_param, tot_validation_value)
+
+    tot_validation_values = np.array(tot_validation_values)
+    best_pen_param = tot_validation_values[np.argmax(tot_validation_values[:,1]),0]
+    return best_pen_param
+
+def fit_to_data(obs_data, pen_param, theta_shape, per_target_model):
+    obs_X, obs_y = get_X_y_matrices(obs_data, per_target_model)
+    logistic_reg = LogisticRegressionMotif(
+            theta_shape,
+            obs_X,
+            obs_y,
+            per_target_model=per_target_model)
+    theta, _ = logistic_reg.solve(pen_param)
+    return theta
 
 def main(args=sys.argv[1:]):
     args = parse_args()
@@ -94,6 +148,7 @@ def main(args=sys.argv[1:]):
         motif_lens=args.motif_lens,
         left_motif_flank_len_list=args.positions_mutating,
     )
+    theta_shape = (feat_generator.feature_vec_len, NUM_NUCLEOTIDES + 1 if args.per_target_model else 1)
 
     log.info("Reading data")
     obs_data, metadata = read_gene_seq_csv_data(
@@ -103,61 +158,57 @@ def main(args=sys.argv[1:]):
         left_flank_len=args.max_left_flank,
         right_flank_len=args.max_right_flank,
     )
+    log.info("num observations %d", len(obs_data))
 
     feat_generator.add_base_features_for_list(obs_data)
     
     # Process data
-    X = []
-    ys = []
-    for i, obs in enumerate(obs_data):
-        X.append(obs.feat_matrix_start)
-        if args.per_target_model:
-            y_vec = np.zeros(obs.seq_len)
-            for k, v in obs.mutation_pos_dict.iteritems():
-                y_vec[k] = NUCLEOTIDE_DICT[v] + 1
-            ys.append(y_vec)
-        else:
-            ys.append(np.array(obs.mutated_indicator))
+    fold_indices = data_split.split(
+        len(obs_data),
+        metadata,
+        args.tuning_sample_ratio,
+        args.k_folds,
+        validation_column=args.validation_col,
+    )
+    data_folds = []
+    for train_idx, val_idx in fold_indices:
+        train_set = [obs_data[i] for i in train_idx]
+        val_set = [obs_data[i] for i in val_idx]
+        train_X, train_y = get_X_y_matrices(train_set, args.per_target_model)
+        val_X, val_y = get_X_y_matrices(val_set, args.per_target_model)
+        logistic_reg = LogisticRegressionMotif(
+                theta_shape,
+                train_X,
+                train_y)
+        data_folds.append((val_X, val_y, logistic_reg))
 
-    stacked_X = scipy.sparse.vstack(X).todense()
-    stacked_y = np.concatenate(ys)
+    # Fit the models for each penalty parameter
+    best_pen_param = get_best_penalty_param(args.penalty_params, data_folds)
+    log.info("best penalty param %f", best_pen_param)
 
-    if args.per_target_model:
-        theta_shape = (stacked_X.shape[1], NUM_NUCLEOTIDES + 1)
-    else:
-        theta_shape = (stacked_X.shape[1], 1)
-    # Fit the model
-    logistic_reg = LogisticRegressionMotif(
+    # Refit penalized with all the data
+    penalized_theta = fit_to_data(
+            obs_data,
+            best_pen_param,
             theta_shape,
-            stacked_X,
-            stacked_y)
-    theta, val = logistic_reg.solve(max_iters=2000)
+            args.per_target_model)
+    model_masks = ModelTruncation(penalized_theta, feat_generator)
+
+    # Create obs data with new pruned feature generator
+    feat_generator_stage2 = copy.deepcopy(feat_generator)
+    feat_generator_stage2.update_feats_after_removing(model_masks)
+    # Refit with all the data -- no penalty!
+    obs_data_stage2 = [copy.deepcopy(o) for o in obs_data]
+    feat_generator_stage2.add_base_features_for_list(obs_data_stage2)
+    refit_theta_shape = (feat_generator_stage2.feature_vec_len, NUM_NUCLEOTIDES + 1 if args.per_target_model else 1)
+    refit_theta = fit_to_data(
+            obs_data_stage2,
+            0,
+            refit_theta_shape,
+            args.per_target_model)
 
     # Aggregate theta
-    full_feat_generator = MotifFeatureGenerator(
-        motif_len=args.max_motif_len,
-        distance_to_start_of_motif=-args.max_left_flank,
-    )
-    hier_full_feat_generator = HierarchicalMotifFeatureGenerator(
-        motif_lens=[args.max_motif_len],
-        left_motif_flank_len_list=[[args.max_left_flank]],
-    )
-    motif_seq_mutations = []
-    for motif in full_feat_generator.motif_list:
-        obs_seq_mut = ObservedSequenceMutations(
-            motif,
-            mutate_string(motif, args.max_left_flank, "b"),
-            motif_len=args.max_motif_len)
-        motif_seq_mutations.append(obs_seq_mut)
-
-    feat_generator.add_base_features_for_list(motif_seq_mutations)
-
-    agg_X = []
-    for motif_idx, motif_obs in enumerate(motif_seq_mutations):
-        agg_X.append(motif_obs.feat_matrix_start)
-    agg_X = scipy.sparse.vstack(agg_X)
-
-    theta_agg = agg_X * theta
+    theta_agg = feat_generator_stage2.create_aggregate_theta(refit_theta)
 
     # Convert theta to log probability of mutation
     if args.per_target_model:
