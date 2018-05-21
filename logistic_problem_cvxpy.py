3645e9073ee805352d9d8ccc2026600f96caa9f7
Jean Feng <jean.feng@gmail.com>
2018-05-21 15:09:14 -0700

    update compare script

--- logistic_problem_cvxpy.py
+++ logistic_problem_cvxpy.py
@@ -10,21 +10,21 @@ class LogisticRegressionMotif:
     def __init__(self, theta_shape, X, y, init_lam=1, per_target_model=False):
         self.per_target_model = per_target_model
         self.theta_intercept = cp.Variable()
-        self.theta = cp.Variable(theta_shape)
+        self.theta = cp.Variable(theta_shape[0], theta_shape[1])
         theta_norm = cp.norm(self.theta, 1)
-        self.lam = cp.Parameter(nonneg=True, value=init_lam)
+        self.lam = cp.Parameter(sign="positive", value=init_lam)
 
-        log_ll = -cp.sum(cp.logistic(-(X * (self.theta[:,0:1] + self.theta_intercept))))
+        log_ll = -cp.sum_entries(cp.logistic(-(X * (self.theta[:,0:1] + self.theta_intercept))))
 
         no_mutate_X = X[y == 0, :]
         no_mutate_numerator = - (no_mutate_X * (self.theta[:,0:1] + self.theta_intercept))
 
-        log_ll = log_ll + cp.sum(no_mutate_numerator)
+        log_ll = log_ll + cp.sum_entries(no_mutate_numerator)
         if per_target_model:
             for i in range(NUM_NUCLEOTIDES):
                 mutate_X_targ = X[y == (i + 1), :]
                 target_ll = -cp.logistic(- (mutate_X_targ * (self.theta[:,i + 1] + self.theta_intercept)))
-                log_ll += cp.sum(target_ll)
+                log_ll += cp.sum_entries(target_ll)
 
         self.problem = cp.Problem(cp.Maximize(log_ll - self.lam * theta_norm))
 
